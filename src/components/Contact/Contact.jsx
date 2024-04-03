c5c073e712eeb92c6f58ec357bed168b470d2054
yamuna-FSD-Developer <yamunag1812@gmail.com>
2024-04-03 23:54:49 +0530

    portfolio

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -7,7 +7,7 @@ export const Contact = () => {
   return (
     <footer id="contact" className={styles.container}>
       <div className={styles.text}>
-        <h2>Contact</h2>
+        <h5>Contact</h5>
         <p>Feel free to reach out!</p>
       </div>
       <ul className={styles.links}>
